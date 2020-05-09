d041caa3b897335ae87969e3a3e09634ca1253d2
Yoga <yogasipahutar20@gmail.com>
2020-05-09 10:02:25 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Just Be Happy" },
-    { author: "standinshd", quotes: "Hey ! Selalu Bahagia" },
-    { author: "standinshd", quotes: "I love you ." },
-    { author: "standinshd", quotes: "If u need me i'll be there." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "MFPS", quotes: "Whatever happens, keep breathing" },
+    { author: "Yogs_", quotes: "Just Be Happy" },
+    { author: "Yogs", quotes: "Hey ! Selalu Bahagia" },
+    { author: "Yogs", quotes: "I love you ." },
+    { author: "Yog", quotes: "If u need me i'll be there." },
+    { author: "Yogs", quotes: "I love you." },
+    { author: "Yogs", quotes: "I'm sorry for loving you." },
 ];
