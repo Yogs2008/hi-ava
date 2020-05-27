daabbe6c21d7370cf4b92f198539b605dfaacfb0
Yoga <yogasipahutar20@gmail.com>
2020-05-27 20:32:53 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,7 +2,7 @@
 export default [
     { author: "Unknown", quotes: "Whatever happens, keep breathing" },
     { author: "Bryan Dison", quotes: "Don’t give up when you still have something to give. Nothing is really over until the moment you stop trying" },
-    { author: "Yogs", quotes: "Hey ! Selalu Bahagia" },
+    { author: "Yogs", quotes: "Hey ! Selalu Bahagiaa" },
     { author: "Yogs", quotes: "I love you ." },
     { author: "Yogs", quotes: "If u need me i'll be there." },
     { author: "Unknown", quotes: "Struggle that you do today is the single way to build a better future" },
