d672ebe47403bc39c3f99565b5ec2c7036701dcf
Yoga <yogasipahutar20@gmail.com>
2020-05-27 20:29:34 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,15 @@
 // prettier-ignore
 export default [
-    { author: "MFPS", quotes: "Whatever happens, keep breathing" },
-    { author: "Yogs_", quotes: "Just Be Happy" },
+    { author: "Unknown", quotes: "Whatever happens, keep breathing" },
+    { author: "Bryan Dison", quotes: "Don’t give up when you still have something to give. Nothing is really over until the moment you stop trying" },
     { author: "Yogs", quotes: "Hey ! Selalu Bahagia" },
     { author: "Yogs", quotes: "I love you ." },
-    { author: "Yog", quotes: "If u need me i'll be there." },
-    { author: "Yogs", quotes: "I love you." },
-    { author: "Yogs", quotes: "I'm sorry for loving you." },
+    { author: "Yogs", quotes: "If u need me i'll be there." },
+    { author: "Unknown", quotes: "Struggle that you do today is the single way to build a better future" },
+    { author: "Chinese Proverbs", quotes: "“Do not take for granted the things closest to your heart. Cling to them as you would with your life, for without them, life is meaningless." },
+    { author: "Mahatma Ghandi", quotes: "The weak can never forgive. Forgiveness is the attribute of the strong." },
+    { author: "Oprah Winfrey", quotes: "Where there is no struggle, there is no strength" },
+    { author: "Yogs", quotes: "If u need me i'll be there." },
+    { author: "Unknown", quotes: "Struggle that you do today is the single way to build a better future" },
+    { author: "Steve Jobs", quotes: "IDon't let the noise of other's opinions drown out your own inner voice." },
 ];
